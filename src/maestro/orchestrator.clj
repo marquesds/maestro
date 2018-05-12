419d82cd726f7326b36b30ee7932e865ebbb1208
Lucas Marques <lmarques.ifsp@gmail.com>
2018-05-12 16:53:48 -0300

    feat(orchestrator): Add job assign's precedence rules

--- src/maestro/orchestrator.clj
+++ src/maestro/orchestrator.clj
@@ -8,13 +8,54 @@
 
 (def job-requests (atom []))
 
-(defn get-entity-from-dict [element]
+(def jobs-assigned (atom #{}))
+
+(defn get-entity [element]
 	(let [nu-agent (get element "new_agent")
 		job (get element "new_job")
 		job-request (get element "job_request")]
 		(first (filter (fn [x] (not (nil? x))) [nu-agent job job-request]))))
 
+(defn get-collection 
+	[element]
+	(cond 
+		(not (nil? (get element "new_agent"))) agents
+		(not (nil? (get element "new_job"))) jobs
+		(not (nil? (get element "job_request"))) job-requests
+		(not (nil? (get element "job_assigned"))) jobs-assigned))
+
+(defn urgent? 
+	[job]
+	(boolean (get job "urgent")))
+
+(defn has-primary-skillset? 
+	[nu-agent job]
+	(.contains (get nu-agent "primary_skillset") (get job "type")))
+
+(defn has-secondary-skillset? 
+	[nu-agent job]
+	(.contains (get nu-agent "secondary_skillset") (get job "type")))
+
+(defn is-urgent-with-primary-skillset? 
+	[nu-agent job]
+	(and (urgent? job) (has-primary-skillset? nu-agent job)))
+
+(defn is-urgent-with-secondary-skillset? 
+	[nu-agent job]
+	(and (urgent? job) (has-secondary-skillset? nu-agent job)))
+
+(defn filter-job
+	[nu-agent jobs filter-fn]
+	(first (filter (fn [job] (filter-fn nu-agent job)) jobs)))
+
+(def precedence-functions [is-urgent-with-primary-skillset? has-primary-skillset? 
+	is-urgent-with-secondary-skillset? has-secondary-skillset?])
+
+(defn get-fittest-job
+	[nu-agent jobs]
+	(first (filter (complement nil?) (map (partial filter-job nu-agent jobs) precedence-functions))))
+
 (defn save-entities
   [input-json]
   (doseq [element input-json]
-  	(save-entity (get-entity-from-dict element))))
+  	(save-entity (get-collection element) (get-entity element))))
--- test/maestro/orchestrator_test.clj
+++ test/maestro/orchestrator_test.clj
@@ -8,24 +8,129 @@
   (do
     (reset! agents #{})
     (reset! jobs #{})
-    (reset! job-requests []))
+    (reset! job-requests [])
+    (reset! jobs-assigned #{}))
   (test-fn))
 
 (use-fixtures :each reset-data)
 
-(deftest test-get-entity-from-dict
+(deftest test-get-entity
 	(let [nu-agent {"new_agent" {"id" "8ab86c18-3fae-4804-bfd9-c3d6e8f66260"}}]
-		(let [entity (get-entity-from-dict nu-agent)]
+		(let [entity (get-entity nu-agent)]
 			(is (= entity (get nu-agent "new_agent"))))))
 
 (deftest test-try-get-not-existent-entity
 	(let [nu-developer {"new_developer" {"id" "8ab86c18-3fae-4804-bfd9-c3d6e8f66260"}}]
-		(let [entity (get-entity-from-dict nu-developer)]
+		(let [entity (get-entity nu-developer)]
 			(is (nil? entity)))))
+
+(deftest test-get-agents-collection
+	(let [collection (get-collection {"new_agent" {"id" 1234}})]
+		(is (= collection agents))))
+
+(deftest test-get-jobs-collection
+	(let [collection (get-collection {"new_job" {"id" 3221}})]
+		(is (= collection jobs))))
+
+(deftest test-get-job-requests-collection
+	(let [collection (get-collection {"job_request" {"id" 9887}})]
+		(is (= collection job-requests))))
+
+(deftest test-get-jobs-assigned-collection
+	(let [collection (get-collection {"job_assigned" {"job_id" 3221 "agent_id" 1234}})]
+		(is (= collection jobs-assigned))))
+
+(deftest test-get-non-existent-collection
+	(let [collection (get-collection {"not_found" {"id" 404}})]
+		(is (nil? collection))))
+
+(deftest test-urgent-job
+	(is (true? (urgent? {"urgent" true}))))
+
+(deftest test-not-urgent-job
+	(is (false? (urgent? {"urgent" false}))))
+
+(deftest test-not-urgent-job-without-urgent-key
+	(is (false? (urgent? {"type" "rewards-question"}))))
+
+(deftest test-agent-has-job-primary-skillset
+	(let [nu-agent {"primary_skillset" ["bills-questions"]}
+		job {"type" "bills-questions"}]
+		(is (true? (has-primary-skillset? nu-agent job)))))
+
+(deftest test-agent-has-not-job-primary-skillset
+	(let [nu-agent {"primary_skillset" ["rewards-question"]}
+		job {"type" "bills-questions"}]
+		(is (false? (has-primary-skillset? nu-agent job)))))
+
+(deftest test-agent-has-job-secondary-skillset
+	(let [nu-agent {"secondary_skillset" ["bills-questions"]}
+		job {"type" "bills-questions"}]
+		(is (true? (has-secondary-skillset? nu-agent job)))))
+
+(deftest test-agent-has-not-job-secondary-skillset
+	(let [nu-agent {"secondary_skillset" ["rewards-question"]}
+		job {"type" "bills-questions"}]
+		(is (false? (has-secondary-skillset? nu-agent job)))))
+
+(deftest test-job-urgent-and-agent-with-job-primary-skillset
+	(let [nu-agent {"primary_skillset" ["bills-questions"]}
+		job {"type" "bills-questions" "urgent" true}]
+		(is (true? (is-urgent-with-primary-skillset? nu-agent job)))))
+
+(deftest test-job-not-urgent-and-agent-with-job-primary-skillset
+	(let [nu-agent {"primary_skillset" ["bills-questions"]}
+		job {"type" "bills-questions" "urgent" false}]
+		(is (false? (is-urgent-with-primary-skillset? nu-agent job)))))
+
+(deftest test-job-urgent-and-agent-with-job-secondary-skillset
+	(let [nu-agent {"primary_skillset" ["rewards-question"] "secondary_skillset" ["bills-questions"]}
+		job {"type" "bills-questions" "urgent" true}]
+		(is (true? (is-urgent-with-secondary-skillset? nu-agent job)))))
+
+(deftest test-job-not-urgent-and-agent-with-job-secondary-skillset
+	(let [nu-agent {"primary_skillset" ["rewards-question"] "secondary_skillset" ["bills-questions"]}
+		job {"type" "bills-questions" "urgent" false}]
+		(is (false? (is-urgent-with-secondary-skillset? nu-agent job)))))
+
+(deftest test-filter-job-with-fn-is-urgent-with-primary-skillset
+	(let [nu-agent {"primary_skillset" ["bills-questions"]}
+		jobs [{"type" "bills-questions" "urgent" false} {"type" "bills-questions" "urgent" true}]]
+		(is (= {"type" "bills-questions" "urgent" true}  (filter-job nu-agent jobs is-urgent-with-primary-skillset?)))))
+
+(deftest test-filter-job-with-fn-is-urgent-with-primary-skillset-not-found
+	(let [nu-agent {"primary_skillset" ["bills-questions"]}
+		jobs [{"id" "1111" "type" "bills-questions" "urgent" false} {"id" "2222" "type" "bills-questions" "urgent" false}]]
+		(is (nil? (filter-job nu-agent jobs is-urgent-with-primary-skillset?)))))
 
 (deftest test-save-entities
 	(let [input-json [{"new_agent" {"id" 1234}} {"new_job" {"id" 3221}} {"job_request" {"id" 9887}}]]
 		(save-entities input-json)
-		(is (= (first @agents) {"new_agent" {"id" 1234}}))
-		(is (= (first @jobs) {"new_job" {"id" 3221}}))
-		(is (= (first @job-requests) {"job_request" {"id" 9887}}))))
+		(is (= (first @agents) {"id" 1234}))
+		(is (= (first @jobs) {"id" 3221}))
+		(is (= (first @job-requests) {"id" 9887}))))
+
+(deftest test-get-fittest-job-that-is-urgent-with-primary-skillset
+	(let [nu-agent {"id" "1234" "primary_skillset" ["bills-questions"] "secondary_skillset" ["rewards-question"]}
+		jobs [{"type" "bills-questions" "urgent" true} {"type" "bills-questions" "urgent" false}]]
+		(is (= {"type" "bills-questions" "urgent" true} (get-fittest-job nu-agent jobs)))))
+
+(deftest test-get-fittest-job-that-is-not-urgent-with-primary-skillset
+	(let [nu-agent {"id" "1234" "primary_skillset" ["bills-questions"] "secondary_skillset" ["rewards-question"]}
+		jobs [{"type" "bills-questions" "urgent" false} {"type" "rewards-question" "urgent" false}]]
+		(is (= {"type" "bills-questions" "urgent" false} (get-fittest-job nu-agent jobs)))))
+
+(deftest test-get-fittest-job-that-is-urgent-with-secondary-skillset
+	(let [nu-agent {"id" "1234" "primary_skillset" ["other-questions"] "secondary_skillset" ["rewards-question"]}
+		jobs [{"type" "bills-questions" "urgent" false} {"type" "rewards-question" "urgent" true}]]
+		(is (= {"type" "rewards-question" "urgent" true} (get-fittest-job nu-agent jobs)))))
+
+(deftest test-get-fittest-job-that-is-not-urgent-with-secondary-skillset
+	(let [nu-agent {"id" "1234" "primary_skillset" ["other-questions"] "secondary_skillset" ["rewards-question"]}
+		jobs [{"type" "bills-questions" "urgent" false} {"type" "rewards-question" "urgent" false}]]
+		(is (= {"type" "rewards-question" "urgent" false} (get-fittest-job nu-agent jobs)))))
+
+(deftest test-get-fittest-job-that-is-not-urgent-with-no-skillset
+	(let [nu-agent {"id" "1234" "primary_skillset" ["other-questions"] "secondary_skillset" ["magic-question"]}
+		jobs [{"type" "bills-questions" "urgent" false} {"type" "rewards-question" "urgent" false}]]
+		(is (nil? (get-fittest-job nu-agent jobs)))))
