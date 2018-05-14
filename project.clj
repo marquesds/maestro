06445d235770ac0e63d1cfcfde5973f96236ab60
Lucas Marques <lmarques.ifsp@gmail.com>
2018-05-13 21:10:37 -0300

    feat(cli): Receive a json with nu-agents, jobs and job requests as input and produce jobs assigned's

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject maestro "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject maestro "1.0.0"
+  :description "Assign fittest job to correct nu-agents"
+  :url "https://gitlab.com/marquesds/maestro/tree/master"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/tools.cli "0.3.7"]]
   :main ^:skip-aot maestro.cli
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/maestro/cli.clj
+++ src/maestro/cli.clj
@@ -1,13 +1,20 @@
 (ns maestro.cli
   (:gen-class)
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json]
+  			[clojure.tools.cli :refer [parse-opts]]
+  			[maestro.orchestrator :refer :all]))
 
-(defn parse-file! 
+
+(def cli-options
+  [["-i" "--input PATH" "Input json"]])
+
+(defn parse-file!
 	[file-path]
 	(let [file-content (slurp file-path)]
     	(lazy-seq (json/read-str file-content))))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (let [options (select-keys (parse-opts args cli-options) [:options])]
+  	(let [input-json-file-path (:input (:options options))]
+  		(let [output (orchestrate (parse-file! input-json-file-path))]
+  			(println (json/write-str output))))))
